d92d24c5a76ed8e05b7aaeaf043dec022f2d3804
Shivamrut <gshivamrut@gmail.com>
2026-03-01 19:42:35 +0530

    phase 1 done
    Signed-off-by: Shivamrut <gshivamrut@gmail.com>

---
+++ libraries/service-api/src/main/java/org/eclipse/sw360/services/health/Health.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Bosch.IO GmbH 2020, 2025. Part of the SW360 Portal Project.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.sw360.services.health;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class Health {
+
+    private Status status;
+    private Map<String, String> details;
+
+    public Health() {
+        this.status = Status.UNKNOWN;
+        this.details = new HashMap<>();
+    }
+}
